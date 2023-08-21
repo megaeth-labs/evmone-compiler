sambacha/evmone

Change has_const_gas_cost() to consteval
This function is not meant to be executed at runtime.

lib/evmone/instructions_traits.hpp
@@ -199,7 +199,7 @@ struct Traits
 /// Determines if an instruction has constant base gas cost across all revisions.
 /// Note that this is not true for instructions with constant base gas cost but
 /// not available in the first revision (e.g. SHL).
-inline constexpr bool has_const_gas_cost(evmc_opcode op) noexcept
+consteval bool has_const_gas_cost(evmc_opcode op) noexcept
 {
     const auto g = gas_costs[EVMC_FRONTIER][op];
     for (size_t r = EVMC_FRONTIER + 1; r <= EVMC_MAX_REVISION; ++r)

megaeth-labs/evmone-compiler

lib/evmone/instructions_traits.hpp
@@ -213,7 +213,7 @@ struct Traits
 /// Determines if an instruction has constant base gas cost across all revisions.
 /// Note that this is not true for instructions with constant base gas cost but
 /// not available in the first revision (e.g. SHL).
-inline constexpr bool has_const_gas_cost(Opcode op) noexcept
+consteval bool has_const_gas_cost(Opcode op) noexcept
 {
     const auto g = gas_costs[EVMC_FRONTIER][op];
     for (size_t r = EVMC_FRONTIER + 1; r <= EVMC_MAX_REVISION; ++r)