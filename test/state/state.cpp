e0ba24a83b87912fe3a3a1a059333aabb43fce87
Paweł Bylica <pawel@ethereum.org>
2023-09-26 12:06:15 +0200

    state: Nice system call

--- test/state/state.cpp
+++ test/state/state.cpp
@@ -146,31 +146,32 @@ void clear_empty_accounts(State& state)
 }
 }  // namespace
 
-void system_calls(State& state, const BlockInfo& block, evmc_revision rev, evmc::VM& vm)
+void system_call(State& state, const BlockInfo& block, evmc_revision rev, evmc::VM& vm)
 {
     static constexpr auto SystemAddress = 0xfffffffffffffffffffffffffffffffffffffffe_address;
     static constexpr auto BeaconRootsAddress = 0xbEac00dDB15f3B6d645C48263dC93862413A222D_address;
 
     if (rev >= EVMC_CANCUN)
     {
-        evmc_message msg{};
-        msg.kind = EVMC_CALL;
-        msg.sender = SystemAddress;
-        msg.recipient = BeaconRootsAddress;
-        msg.code_address = BeaconRootsAddress;
-        msg.gas = 30'000'000;
-        msg.input_data = block.parent_beacon_block_root.bytes;
-        msg.input_size = sizeof(block.parent_beacon_block_root);
-        msg.depth = 1;  // Prevent nonce bump.
-        Host host{rev, vm, state, block, {}};
-        [[maybe_unused]] const auto result = host.call(msg);
-        assert(result.status_code == EVMC_SUCCESS);
-
-        // Set accounts and their storage access status to cold in the end of transition process
-        for (auto& acc : state.get_accounts())
+        if (const auto acc = state.find(BeaconRootsAddress); acc != nullptr)
         {
-            acc.second.access_status = EVMC_ACCESS_COLD;
-            for (auto& [_, val] : acc.second.storage)
+            const evmc_message msg{
+                .kind = EVMC_CALL,
+                .gas = 30'000'000,
+                .recipient = BeaconRootsAddress,
+                .sender = SystemAddress,
+                .input_data = block.parent_beacon_block_root.bytes,
+                .input_size = sizeof(block.parent_beacon_block_root),
+            };
+
+            Host host{rev, vm, state, block, {}};
+            const auto& code = acc->code;
+            [[maybe_unused]] const auto res = vm.execute(host, rev, msg, code.data(), code.size());
+            assert(res.status_code == EVMC_SUCCESS);
+            assert(acc->access_status == EVMC_ACCESS_COLD);
+
+            // Reset storage status.
+            for (auto& [_, val] : acc->storage)
             {
                 val.access_status = EVMC_ACCESS_COLD;
                 val.original = val.current;
